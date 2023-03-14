7f5e5418faf6145c3c67ad9703e2bb3afdd197bd
veenakaushik132000 <vrani033@gmail.com>
2023-03-14 18:01:58 +0530

    adding

--- PythonFiles/first.py
+++ PythonFiles/first.py
@@ -1,4 +1,11 @@
-print("Hello Python")
-a=10
-b=20
-print(a+b)
+# print("Hello Python")
+# a=10
+# b=20
+# print(a+b)
+
+
+import sys
+
+a=int(sys.argv[1])
+b=int(sys.argv[2])
+print('Result',(a+b))
